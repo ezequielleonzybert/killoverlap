cdba1b3edb317f8d38d07980708ad13bb8271f72
ezequiel <ezequielleonzybert@gmail.com>
2024-05-27 17:41:10 -0300

    finish

--- killoverlap2.py
+++ killoverlap2.py
@@ -1,14 +1,14 @@
 from build123d import *
 
-input = import_svg("input.svg")
+input = import_svg("test.svg")
 
-with BuildPart() as part:
+with BuildLine() as lines:
     for edge in input.edges():
         add(edge)
 
-shape = part.pending_edges_as_wire
+shape = lines._obj
 
 exporter = ExportSVG()
-exporter.add_layer(name="layer1", line_color=(255,0,0))
+exporter.add_layer(name="layer1", line_color=(255,0,0),line_weight=1)
 exporter.add_shape(shape,"layer1")
 exporter.write("output.svg")
